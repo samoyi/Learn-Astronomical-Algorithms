cf581687b9ba355767f95e3b8c24ac4c1aa09752
samoyi <greatlining@163.com>
2017-09-30 12:51:37 +0800

    正在练习用Express

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 ;"use strict";
 
+/*
+ 巫术
+ 占卜
+ 天文
+ 历法
+ 阴阳两界
+*/
+
+
 // const http = require('http');
 //
 // http.createServer((req, res)=>{
@@ -10,16 +19,36 @@
 //     res.end();
 // }).listen(3000);
 
-const http = require('http'),
-      url = require('url'),
-      util = require('util');
 
-const server = new http.Server();
-server.on('request', function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-  res.write('<h1>陰陽師</h1>');
-  res.end(util.inspect(url.parse(req.url, true)));
+var express = require('express');
+
+var app = express.createServer();
+app.use(express.bodyParser());
+app.all('/', function(req, res) {
+    res.send(req.body.title + req.body.text);
 });
-server.listen(3000);
 
-console.log("HTTP server is listening at port 3000.");
+app.listen(3000);
+
+
+
+// const http = require('http'),
+//       url = require('url'),
+//       util = require('util');
+//
+// const server = new http.Server();
+// server.on('request', function(req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+//   res.write('<h1>陰陽師</h1>');
+//   res.write( url.parse(req.url).query );
+//   res.write('<br />');
+//   res.write( util.inspect(url.parse(req.url)) );
+//   res.write('<br />');
+//   res.write( typeof url.parse(req.url, true).query );
+//   res.write('<br />');
+//   res.write( util.inspect(url.parse(req.url, true)) );
+//   res.end();
+// });
+// server.listen(3000);
+//
+// console.log("HTTP server is listening at port 3000.");
